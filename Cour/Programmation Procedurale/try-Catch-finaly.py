f227bc7a9f62dd6eafb24bbe3cb754d1ec99804d
Unknown <yossefhilleltapiero@hotmail.fr>
2019-05-30 22:42:56 +0300

    try exception

--- Cour/Programmation Procedurale/try-Catch-finaly.py
+++ Cour/Programmation Procedurale/try-Catch-finaly.py
@@ -1,9 +1,28 @@
 # exception 
 '''
+lorsque l interpreteur detecte une code ou un code deffectueux(commande impossible, variable non implementer,...)
+il leve ce qu on apelle une exception pour faire remonter l erreur 
+une exception est un objet compose dun nom et d'une explication de l erreur
+
+ex: ZeroDivisionError: int division or modulo by zero
+
 '''
-#Try 
+# Try-Catch(Except) 
 '''
-lorsque l interpreteur detecte une code ou un code deffectueux(commande impossible )
+try sert a 'essayer' une un bloc d instruction
+il teste si la commande est possible et executable
+si elle l est le programme continue normalement
+si elle ne l est pas ine exception est levee elle est atrapper par le bloc catch 
+le bloc dinstruction de catch s execute et le programme continue sont execution normalement apres
+les try sont emboitable 
+
+    declaration:
+        1:  try:
+                cmd1         
+                cmd2
+            except: 
+                cmd1         
+                cmd2
 '''
 # Catch
 '''
