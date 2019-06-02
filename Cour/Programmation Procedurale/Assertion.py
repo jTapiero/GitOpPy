045fde7d645d2e62179c937ca699e4e966859080
Dextech <yossefhilleltapiero@hotmail.fr>
2019-06-02 17:24:33 +0300

    exception

---
+++ Cour/Programmation Procedurale/Assertion.py
@@ -0,0 +1,10 @@
+#Assertion
+'''
+une assertion est une regle qu on fixe dans le code 
+le respect de cette regle veut que son etat est vrai 
+le Non respect de cette regle veut que son etat est False
+en cas de non respect de cette regle une exception est lever le: AssertionError
+ declaration:
+    assert [Predicat/var/Boolean]
+    
+'''
